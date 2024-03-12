b2ef49a55c302b701fa9f00eb24f368cfffab6ee
Dr Bill Close <90579801+bill-close@users.noreply.github.com>
2024-03-13 09:32:02 +1000

    update

--- workspace_from_code.py
+++ workspace_from_code.py
@@ -8,13 +8,12 @@ print(azureml.core.VERSION)
 
 # Create the workspace using the specified parameters
 
-sub = "90884d79-68d2-4ee8-b32f-9aa53a9a3c4f"
-forced_interactive_auth = InteractiveLoginAuthentication(tenant_id="dedd61ab-b77d-444d-8289-15e9ab84ca0f", force=True)
+SUBSCRIPTION = "<your-subscription-id>"
+forced_interactive_auth = InteractiveLoginAuthentication(tenant_id="<your-tenant-id>", force=True)
 
-ws = Workspace.create(name='azureml_experiments_workspace',
-                      subscription_id=sub,
-                      resource_group='azureml_experiments_resource',
+ws = Workspace.create(name='<name_your_workspace>',
+                      subscription_id=SUBSCRIPTION,
+                      resource_group='<name_your_resource>',
                       create_resource_group=True,
                       location='australiaeast'
                      )
-
